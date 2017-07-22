c01678ecaabb6738eba31bf2c9c58e582d29d63d
TK <t@K.pl>
2017-07-22 11:36:14 +0200

    prawie ok

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+﻿$(function(){
+	var carouselList = $("#carousel ul");
+	setTimeout(changeSlide, 3000);
+
+	function changeSlide(){
+		carouselList.animate({"marginLeft":-400}, 500, moveFirstSlide);
+	}
+
+	function moveFirstSlide(){
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft:0});
+	}
+	
+});
